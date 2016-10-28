6dd672649c8a5fa4394474a0f9541e90750b15ee
mposso7 <micposso@gmail.com>
2016-10-27 22:20:40 -0400

    setting up the render.js file

---
+++ js/render.js
@@ -0,0 +1,11 @@
+//function that handdles the reading of the files and merge in one
+
+function view(templateName, values, response ){
+  //read from template
+  
+  //insert values into the content
+  
+  //write out the response
+  
+  
+}
