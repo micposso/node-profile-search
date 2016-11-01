b1a285023ede458388d8b30959d061ef9197fc7c
michael posso <mposso@babbel.com>
2016-11-01 08:26:26 -0500

    return key values

--- js/render.js
+++ js/render.js
@@ -1,6 +1,14 @@
 //function that handdles the reading of the files and merge in one
-
 var fs = require('fs');
+
+function mergeValues(values, content){
+  //cycle over the keys
+  for(var key in values){
+    content = content.replace("{{+ key +}}", values[key])
+  }
+  //return merge content
+  return content;
+}
 
 function view(templateName, values, response ){
   //read from template
